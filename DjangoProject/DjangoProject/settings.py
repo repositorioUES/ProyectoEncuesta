9e5f7be3c1bcc711e68acca298b37667036042d6
Christian Garcia <67031790+repositorioUES@users.noreply.github.com>
2020-06-25 04:36:18 -0600

    configuracion de DB y settings.py

--- DjangoProject/DjangoProject/settings.py
+++ DjangoProject/DjangoProject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ProTransApp',
 ]
 
 MIDDLEWARE = [
@@ -107,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-SV'
 
 TIME_ZONE = 'UTC'
 
