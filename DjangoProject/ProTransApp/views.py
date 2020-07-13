7fb17cf5bacb108821e07ab6ae041872113201d7
Christian Garcia <67031790+repositorioUES@users.noreply.github.com>
2020-07-13 12:33:14 -0600

    (CZ13016) Se ha coompletado el crud guardar encuesta

--- DjangoProject/ProTransApp/views.py
+++ DjangoProject/ProTransApp/views.py
@@ -10,15 +10,14 @@ from django.shortcuts import redirect
 class Inicio(TemplateView):
 	template_name = 'index.html'
 
-#dui = "n";
-	noDui = ""
 
+#noDui = ""
 class UsuarioCrear(CreateView):
 	model = Usuario
 	form_class = UsuarioForm
 	template_name = 'Usuario_Form.html'
 	def post(self, request, *args, **kwargs):
-		global noDui
+		#global noDui
 		noDui = request.POST.get('dui') 
 		nom = request.POST.get('nombre')
 		apel = request.POST.get('apellidos')
@@ -26,81 +25,95 @@ class UsuarioCrear(CreateView):
 		sex = request.POST.get('sexo')
 		dom = request.POST.get('domicilio')
 		tipo = request.POST.get('tipo_transporte')
-		print(request.POST.get('dui'), "CAMBIO",tipo)
 		ipoTrasporte = Tipo_transporte.objects.get(tipo_transporte = tipo)		
 		usuario = Usuario(dui = noDui, nombre = nom, apellidos = apel, edad = ed, sexo = sex, domicilio = dom, tipo_transporte = ipoTrasporte)
 		usuario.save()
+		obtenerDui(request)
 		return redirect('llenar_e')
 
+def obtenerDui(request):
+	global numeroDui
+	numeroDui = request.POST.get('dui') 
+	
 def Encuesta(request):
 		if request.method == 'POST':
-			noPregunta = request.POST.get('numerodepregunta')
 			guardarDatosEncuesta(request)
+			print(numeroDui, "CAMBIO")
 			return redirect('inicio')
 		return render(request, 'llenarE.html')
-				
+
 def guardarDatosEncuesta(request):
+	usuario = Usuario.objects.get(dui =numeroDui)
 	pregunta1 = request.POST.get('p1')
 	if pregunta1 != None:
-		usuario = Usuario.objects.get(dui = '99999999')
-		respuesta = Respuesta(dui = usuario, numerodepregunta = pregunta1)
-		respuesta.save()
-		print(pregunta1, 'EL VALOR ES CORRECTO')
+		respuesta1 = Respuesta.objects.filter(dui =numeroDui).filter(numerodepregunta = pregunta1).exists()
+		if respuesta1 != True:
+			print(respuesta1,'FILTRO OBTENIDO')
+			respuesta = Respuesta(dui = usuario, numerodepregunta = pregunta1)
+			respuesta.save()
 	
 	pregunta2 = request.POST.get('p2')
 	if pregunta2 != None:
-		usuario = Usuario.objects.get(dui = '99999999')
-		respuesta = Respuesta(dui = usuario, numerodepregunta = pregunta2)
-		respuesta.save()
-		print(pregunta2, 'EL VALOR ES CORRECTO')
+		respuesta2 = Respuesta.objects.filter(dui =numeroDui).filter(numerodepregunta = pregunta2).exists()
+		if respuesta2 != True:
+			respuesta = Respuesta(dui = usuario, numerodepregunta = pregunta2)
+			respuesta.save()
+			print(pregunta2, 'EL VALOR ES CORRECTO')
 	
 	pregunta3 = request.POST.get('p3')
 	if pregunta3 != None:
-		usuario = Usuario.objects.get(dui = '99999999')
-		respuesta = Respuesta(dui = usuario, numerodepregunta = pregunta3)
-		respuesta.save()
-		print(pregunta3, 'EL VALOR ES CORRECTO')
+		respuesta3 = Respuesta.objects.filter(dui =numeroDui).filter(numerodepregunta = pregunta3).exists()
+		if respuesta3 != True:
+			respuesta = Respuesta(dui = usuario, numerodepregunta = pregunta3)
+			respuesta.save()
+			print(pregunta3, 'EL VALOR ES CORRECTO')
 	
 	pregunta4 = request.POST.get('p4')
 	if pregunta4 != None:
-		usuario = Usuario.objects.get(dui = '99999999')
-		respuesta = Respuesta(dui = usuario, numerodepregunta = pregunta4)
-		respuesta.save()
-		print(pregunta4, 'EL VALOR ES CORRECTO')
+		respuesta4 = Respuesta.objects.filter(dui =numeroDui).filter(numerodepregunta = pregunta4).exists()
+		if respuesta4 != True:
+			respuesta = Respuesta(dui = usuario, numerodepregunta = pregunta4)
+			respuesta.save()
+			print(pregunta4, 'EL VALOR ES CORRECTO')
 	
 	pregunta5 = request.POST.get('p5')
 	if pregunta5 != None:
-		usuario = Usuario.objects.get(dui = '99999999')
-		respuesta = Respuesta(dui = usuario, numerodepregunta = pregunta5)
-		respuesta.save()
-		print(pregunta5, 'EL VALOR ES CORRECTO')
+		respuesta5 = Respuesta.objects.filter(dui =numeroDui).filter(numerodepregunta = pregunta5).exists()
+		if respuesta5 != True:
+			respuesta = Respuesta(dui = usuario, numerodepregunta = pregunta5)
+			respuesta.save()
+			print(pregunta5, 'EL VALOR ES CORRECTO')
 	
 	pregunta6 = request.POST.get('p6')
 	if pregunta6 != None:
-		usuario = Usuario.objects.get(dui = '99999999')
-		respuesta = Respuesta(dui = usuario, numerodepregunta = pregunta6)
-		respuesta.save()
-		print(pregunta6, 'EL VALOR ES CORRECTO')
+		respuesta6 = Respuesta.objects.filter(dui =numeroDui).filter(numerodepregunta = pregunta6).exists()
+		if respuesta6 != True:
+			respuesta = Respuesta(dui = usuario, numerodepregunta = pregunta6)
+			respuesta.save()
+			print(pregunta6, 'EL VALOR ES CORRECTO')
 	
 	pregunta7 = request.POST.get('p7')
 	if pregunta7 != None:
-		usuario = Usuario.objects.get(dui = '99999999')
-		respuesta = Respuesta(dui = usuario, numerodepregunta = pregunta7)
-		respuesta.save()
-		print(pregunta7, 'EL VALOR ES CORRECTO')
+		respuesta7 = Respuesta.objects.filter(dui =numeroDui).filter(numerodepregunta = pregunta7).exists()
+		if respuesta7 != True:
+			respuesta = Respuesta(dui = usuario, numerodepregunta = pregunta7)
+			respuesta.save()
+			print(pregunta7, 'EL VALOR ES CORRECTO')
 	
 	pregunta8 = request.POST.get('p8')
 	if pregunta8 != None:
-		usuario = Usuario.objects.get(dui = '99999999')
-		respuesta = Respuesta(dui = usuario, numerodepregunta = pregunta8)
-		respuesta.save()
-		print(pregunta8, 'EL VALOR ES CORRECTO')
+		respuesta8 = Respuesta.objects.filter(dui =numeroDui).filter(numerodepregunta = pregunta8).exists()
+		if respuesta8 != True:
+			respuesta = Respuesta(dui = usuario, numerodepregunta = pregunta8)
+			respuesta.save()
+			print(pregunta8, 'EL VALOR ES CORRECTO')
 	
 	pregunta9 = request.POST.get('p9')
 	if pregunta9 != None:
-		usuario = Usuario.objects.get(dui = '99999999')
-		respuesta = Respuesta(dui = usuario, numerodepregunta = pregunta9)
-		respuesta.save()
-		print(pregunta9, 'EL VALOR ES CORRECTO')
-	
+		respuesta9 = Respuesta.objects.filter(dui =numeroDui).filter(numerodepregunta = pregunta9).exists()
+		if respuesta9 != True:
+			respuesta = Respuesta(dui = usuario, numerodepregunta = pregunta9)
+			respuesta.save()
+			print(pregunta9, 'EL VALOR ES CORRECTO')
+		
 
